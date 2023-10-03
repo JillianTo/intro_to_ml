753a6d2ea70b31fb0c8ac767b59de659351436a9
Jillian To <jtoschool388@gmail.com>
2023-10-03 14:31:59 -0400

    added loaded to pytorch example

--- pytorch_example.py
+++ pytorch_example.py
@@ -1,8 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
 from torch.utils.data import DataLoader
 import torchvision
 import torchvision.transforms as transforms
 import wandb
 from tqdm import tqdm
 
+wandb.int(project="mnist.pytorch", entity="jto")
+
 print("Loading MNIST dataset without normalization...")
-trainset=torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
+trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
+trainloader = DataLoader(trainset, batch_size=1000, shuffle=False)
